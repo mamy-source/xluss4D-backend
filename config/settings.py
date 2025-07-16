6a7b45c0631bcee9f8981c9bbd39eeeb1cce30d6
Tanjona <andrianiainatanjona67@gmail.com>
2025-07-16 13:58:54 +0300

    test

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,  # pour les performances en production
-    )
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 # Password validation
