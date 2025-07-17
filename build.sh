fbaa09bbd3a56d9451000956853305f3ae7aded3
Tanjona <andrianiainatanjona67@gmail.com>
2025-07-17 15:04:15 +0300

    switch to postgresql

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Install dependencies (optional if Render already does this)
-pip install -r requirements.txt
-
 # Apply migrations
 python manage.py migrate
 
-# Collect static files (if needed)
-python manage.py collectstatic --noinput
