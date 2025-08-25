329b8ac8c4253291cc3c9f559f5c467abd393a5a
Tanjona <andrianiainatanjona67@gmail.com>
2025-08-25 15:28:39 +0300

    model produits

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class UserProfile(models.Model):
     """Profil utilisateur étendu depuis User (par défaut Django)"""
     ROLE_TYPES = (
@@ -34,3 +35,35 @@ class UserRefreshToken(models.Model):
     def __str__(self):
         return f"Refresh token for {self.user.username}"
 
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    subcategory = models.ForeignKey(
+        SubCategory, related_name="products", on_delete=models.CASCADE
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="images", on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return f"Image for {self.product.title}"
