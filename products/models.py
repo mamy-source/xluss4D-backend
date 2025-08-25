8c701820ad2f57d56cc0c25182f4d19c5380e811
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-08-25 15:22:19 +0300

    model category and subcategory

--- products/models.py
+++ products/models.py
@@ -34,3 +34,26 @@ class UserRefreshToken(models.Model):
     def __str__(self):
         return f"Refresh token for {self.user.username}"
 
+
+class Category(models.Model):
+    name =  models.CharField(max_length=255, unique=True),
+    image = models.ImageField(upload_to="categories",blank=True, null=True),
+    created_at = models.DateTimeField(auto_now_add=True),
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"category of {self.name}"
+
+class SubCategory(models.Model):
+    name =  models.CharField(max_length=255, unique=True),
+    image = models.ImageField(upload_to="subcategories",blank=True, null=True),
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcateries')
+    created_at = models.DateTimeField(auto_now_add=True),
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("Category", "name")
+
+    def __str__(self):
+        return f"sub_category of {self.category.name}"
+
